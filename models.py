8aff55f6f30ed7bcdad30f21cf70952a66a973ec
Archana-Balasubramanian <archana1462005@gmail.com>
2025-08-21 18:45:11 +0530

    User Modal Created

--- models.py
+++ models.py
@@ -3,8 +3,21 @@ from uuid import uuid4
 from datetime import datetime
 
 
-class Role:
+class Role(Document):
     id = StringField(primary_key=True, default=lambda:str(uuid4()))  
     name = StringField(unique=True, required=True)
     addedTime = DateTimeField(default=datetime.now())
     updatedTime = DateTimeField()
+
+class User(Document):
+    id = StringField(primary_key=True, default=lambda:str(uuid4()))    
+    username = StringField(unique=True, required=True)
+    email = EmailField(unique=True, required=True)
+    password = StringField(required=True)
+    phone = StringField()
+    role = ReferenceField(Role, null=True)
+    addedTime = DateTimeField(default=datetime.now())
+    updatedTime = DateTimeField()
+
+    
+    
